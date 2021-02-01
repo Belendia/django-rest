835d5332d9e46a0220a8eff4ac1d948a3da8d1de
Belendia Serda <belendia@gmail.com>
2021-02-01 12:05:09 +0300

    Add utils app to define common data model

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users'
+    'apps.users',
+    'apps.utils',
+
 ]
 
 MIDDLEWARE = [
